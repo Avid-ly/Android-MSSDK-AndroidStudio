2d6fa32ab476c076da73837c480375ce970bce4d
wanlei <roy.wan@upltv.com>
2020-05-14 16:49:58 +0800

    1.升级到10023，追加gdpr弹窗的操作

--- app/src/main/java/com/avidly/mssdk/demo/MainAppActivity.java
+++ app/src/main/java/com/avidly/mssdk/demo/MainAppActivity.java
@@ -2,6 +2,7 @@ package com.avidly.mssdk.demo;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 
@@ -10,14 +11,18 @@ import androidx.core.content.ContextCompat;
 
 import com.aly.sdk.ALYAnalysis;
 import com.ms.sdk.MsSDK;
+import com.ms.sdk.listener.MsSdkInitializationListener;
+import com.ms.sdk.listener.MssdkConsentDialogListener;
 
 ;
 
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class MainAppActivity extends BaseActivity {
+    private static final String TAG="MSSdk_demo";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (ContextCompat.checkSelfPermission(MainAppActivity.this, WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
@@ -28,8 +33,28 @@ public class MainAppActivity extends BaseActivity {
         // 上线时请关闭
         MsSDK.setDebuggable(true);
         // 同意GDPR
-        MsSDK.grantConsent(this);
-        MsSDK.init(this);
+//        MsSDK.grantConsent(this);
+         MsSDK.init(this, new MsSdkInitializationListener() {
+            @Override
+            public void onInitializationFinished() {
+                Log.i(TAG, "onInitializationFinished: ");
+                // 如果所处地区受GDPR条约的约束,且没有设置过授权结果
+                if (MsSDK.shouldShowGDPRConsentDialog()){
+                    MsSDK.showConsentDialog(new MssdkConsentDialogListener() {
+                        @Override
+                        public void consentSuccess(boolean agreeConsent) {
+                            Log.i(TAG, "consentResult: "+agreeConsent);
+                        }
+
+                        @Override
+                        public void consentFail(String message) {
+                            Log.i(TAG, "consentFail: "+message);
+                        }
+                    });
+                }
+
+            }
+        });
 
         findViewById(R.id.btnVideo).setOnClickListener(new View.OnClickListener() {
             @Override
